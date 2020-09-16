312b925fad633fbb937f6771c7168c4dc5a89535
ayase <paper.airplane01@gmail.com>
2020-09-16 15:42:47 +0900

    Update circular shift test.

--- test/bit/main.cpp
+++ test/bit/main.cpp
@@ -4,30 +4,32 @@
 #include <catch2/catch.hpp>
 
 TEST_CASE("Circular shift") {
-  REQUIRE(bit::rotl(16, 2) == 64);
-  REQUIRE(bit::rotr(16, 2) == 4);
-
-  constexpr std::uint8_t v0 = 0b1001'0110;
-
-  constexpr auto v1 = bit::rotl(v0, 2);
-  REQUIRE(v1 == 0b0101'1010);
-
-  constexpr auto v2 = bit::rotl(v0, 3);
-  REQUIRE(v2 == 0b1011'0100);
-
-  constexpr auto v3 = bit::rotr(v2, 5);
-  REQUIRE(v3 == 0b1010'0101);
-
-  constexpr auto v4 = bit::rotr(v3, 1);
-  REQUIRE(v4 == 0b1101'0010);
-
-  constexpr auto v5 = bit::rotl(v4, 3);
-  REQUIRE(v5 == 0b1001'0110);
+  constexpr std::uint8_t seq = 0b1001'0110;
+  SECTION("Left circular shift") {
+    REQUIRE(bit::rotl(seq, 1) == 0b0010'1101);
+    REQUIRE(bit::rotl(seq, 2) == 0b0101'1010);
+    REQUIRE(bit::rotl(seq, 3) == 0b1011'0100);
+    REQUIRE(bit::rotl(seq, 4) == 0b0110'1001);
+    REQUIRE(bit::rotl(seq, 5) == 0b1101'0010);
+    REQUIRE(bit::rotl(seq, 6) == 0b1010'0101);
+    REQUIRE(bit::rotl(seq, 7) == 0b0100'1011);
+    REQUIRE(bit::rotl(seq, 8) == 0b1001'0110);
+  }
+  SECTION("Right circular shift") {
+    REQUIRE(bit::rotr(seq, 1) == 0b0100'1011);
+    REQUIRE(bit::rotr(seq, 2) == 0b1010'0101);
+    REQUIRE(bit::rotr(seq, 3) == 0b1101'0010);
+    REQUIRE(bit::rotr(seq, 4) == 0b0110'1001);
+    REQUIRE(bit::rotr(seq, 5) == 0b1011'0100);
+    REQUIRE(bit::rotr(seq, 6) == 0b0101'1010);
+    REQUIRE(bit::rotr(seq, 7) == 0b0010'1101);
+    REQUIRE(bit::rotr(seq, 8) == 0b1001'0110);
+  }
 }
 
 TEST_CASE("Number of Leading Zero (NLZ)") {
   REQUIRE(bit::nlz(0b0) == 32);
-  REQUIRE(bit::nlz(0b01) == 31);
+  REQUIRE(bit::nlz(0b1) == 31);
   REQUIRE(bit::nlz(0b0000'0000'0000'0000'1000'0000'0000'1000) == 16);
   REQUIRE(bit::nlz(0b0000'0000'0000'0000'1000'0000'0000'1000) ==
           bit::nlz(0b1000'0000'0000'1000));
