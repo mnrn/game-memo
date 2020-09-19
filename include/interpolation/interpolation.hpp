344ca47e2e4eb35d5bc20109f1257d05ae3c2780
ayase <paper.airplane01@gmail.com>
2020-09-20 00:16:40 +0900

    Use decltype in static_cast.

--- include/interpolation/interpolation.hpp
+++ include/interpolation/interpolation.hpp
@@ -50,7 +50,9 @@ template <typename T1, typename T2, typename T3>
 constexpr decltype(auto) smoothstep(const T1 &edge0, const T2 &edge1,
                                     const T3 &x) {
   constexpr decltype(auto) t =
-      std::clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0);
+      std::clamp((x - edge0) / (edge1 - edge0),
+                 static_cast<decltype((x - edge0) / (edge1 - edge0))>(0.0),
+                 static_cast<decltype((x - edge0) / (edge1 - edge0))>(1.0));
   return t * t * (3.0 - 2.0 * t);
 }
 
