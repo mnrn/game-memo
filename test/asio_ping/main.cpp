039b17ee27d298b54898097c01ca08ee25b8d1a0
ayase <paper.airplane01@gmail.com>
2020-10-06 10:27:50 +0900

    Rename ping to asio_ping.

---
+++ test/asio_ping/main.cpp
@@ -0,0 +1,21 @@
+#include <boost/asio.hpp>
+#include <iostream>
+
+#include "experimental/network/ping.hpp"
+
+int main(int argc, char *argv[]) {
+  try {
+    if (argc != 2) {
+      std::cerr << "Usage: ping <host>" << std::endl;
+#if !defined(BOOST_ASIO_WINDOWS)
+      std::cerr << "(You may need to run this program as root.)" << std::endl;
+#endif
+      return 1;
+    }
+    boost::asio::io_context io_context;
+    net::pinger p(io_context, argv[1]);
+    io_context.run();
+  } catch (std::exception &e) {
+    std::cerr << "Exception: " << e.what() << std::endl;
+  }
+}
